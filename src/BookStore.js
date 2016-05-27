a0cc017470166b20922b7444f8cc8fba76f2a8c1
Josh Woodward <github@joshw.org>
2016-05-27 14:37:21 -0400

    New

--- src/BookStore.js
+++ src/BookStore.js
@@ -76,14 +76,93 @@ var BookList = React.createClass({
 });
 
 var ShippingDetails = React.createClass({
+    getInitialState() {
+        return {
+            fullName: '', contactNumber: '', shippingAddress: '', error: false
+        };
+    },
+    _renderError() {
+        if( this.state.error )
+            return <div className="alert alert-danger">{this.state.error}</div>;
+    },
+    _validateInput() {
+        if( this.state.fullName === '' ) {
+            this.setState({error: "Please enter your full name."});
+        } else if( this.state.contactNumber === '' ) {
+            this.setState({error: "Please enter your contact number."});
+        } else if( this.state.shippingAddress === '' ) {
+            this.setState({error: "Please enter your shipping address."});
+        } else {
+            this.setState({error: false});
+            return true;
+        }
+    },
+    handleSubmit(event) {
+        event.preventDefault();
+        var formData = {
+            fullName: this.state.fullName, contactNumber: this.state.contactNumber, shippingAddress: this.state.shippingAddress
+        };
+        if( this._validateInput() )
+            this.props.updateFormData(formData);
+    },
+    handleChange(event, attribute) {
+        var newState = this.state;
+        newState[attribute] = event.target.value;
+        this.setState(newState);
+        console.log(this.state);
+    },
     render() {
-        return <h1>Enter shipping info:</h1>;
+        var errorMessage = this._renderError();
+        return <div>
+            <h1>Enter shipping info:</h1>
+            {errorMessage}
+
+            <div style={{width: 200}}>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group"> <input className="form-control" type="text" placeholder="Full Name" value={this.state.fullName} onChange={(event) => this.handleChange(event, 'fullName')} /> </div>
+                    <div className="form-group"> <input className="form-control" type="text" placeholder="Contact number" value={this.state.contactNumber} onChange={(event) => this.handleChange(event, 'contactNumber')}/> </div>
+                    <div className="form-group"> <input className="form-control" type="text" placeholder="Shipping Address" value={this.state.shippingAddress} onChange={(event) => this.handleChange(event, 'shippingAddress')} /> </div>
+                    <div className="form-group"> <button type="submit" ref="submit" className="btn btn-success"> Submit </button></div>
+                </form>
+            </div>
+        </div>;
     }
 });
 
 var DeliveryDetails = React.createClass({
+    getInitialState() {
+        return {
+            deliveryOption: 'Primary'
+        }
+    },
+    handleChange(event) {
+        this.setState({deliveryOption: event.target.value});
+    },
+    handleSubmit(event) {
+        event.preventDefault();
+        this.props.updateFormData({deliveryOption: this.state.deliveryOption});
+    },
     render() {
-        return <h1>Choose delivery option:</h1>;
+        return <div>
+            <h1>Choose delivery option:</h1>
+
+            <form onSubmit={this.handleSubmit}>
+            <select onChange={(event) => this.handleChange(event)}>
+                <option value="Primary">Primary</option>
+                <option value="Normal">Normal</option>
+            </select>
+
+            <input type="submit" value="Save" />
+            </form>
+        </div>;
+    }
+});
+
+var Confirmation = React.createClass({
+    render() {
+        return <div>
+            Thanks! {this.props.data.fullName}
+        </div>;
     }
 });
 
@@ -93,7 +172,7 @@ var BookStore = React.createClass({
     },
     updateFormData(formData) {
         var formValues = Object.assign({}, this.state.formValues, formData);
-        var currentStep = Math.max(this.state.currentStep+1, 3);
+        var currentStep = Math.min(this.state.currentStep+1, 4);
         this.setState({formValues: formValues, currentStep: currentStep});
     },
     render() {
@@ -104,6 +183,8 @@ var BookStore = React.createClass({
                 return <ShippingDetails updateFormData={this.updateFormData} />;
             case 3:
                 return <DeliveryDetails updateFormData={this.updateFormData} />;
+            case 4:
+                return <Confirmation data={this.state.formValues} />;
         }
     }
 });
